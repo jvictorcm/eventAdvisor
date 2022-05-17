4d55c12830cef50b361c741a93536501f70617fc
jvictorcm <9091177+jvictorcm@users.noreply.github.com>
2022-05-16 21:06:35 -0300

    spring initialzr

---
+++ src/main/java/de/victor/eventadvisor/EventAdvisorApplication.java
@@ -0,0 +1,13 @@
+package de.victor.eventadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/victor/eventadvisor/EventAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.victor.eventadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
